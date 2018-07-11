9abd0d6afebf54d9cc4e4a81fd6789b66c7e0909
MutumaMutuma <lewismutuma1000@gmail.com>
2018-07-11 09:24:18 +0300

    initial commit

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $("h1").click(function() {
+      alert("This is a header.");
+    });
+
+    $("img").click(function() {
+      alert("This is an image.");
+    });
+  });
